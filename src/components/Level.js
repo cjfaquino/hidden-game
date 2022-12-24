62c37235946e8036d3514d7ea7653e7854cdd4b3
Christian Aquino <cjfaquino@gmail.com>
2022-12-23 23:10:59 -0800

    add levels & imgs

---
+++ src/components/Level.js
@@ -0,0 +1,11 @@
+class Level {
+  constructor(name, imgUrl, pos1, pos2, pos3) {
+    this.name = name;
+    this.imgUrl = imgUrl;
+    this.pos1 = pos1;
+    this.pos2 = pos2;
+    this.pos3 = pos3;
+  }
+}
+
+export default Level;
---
+++ src/components/levels.js
@@ -0,0 +1,13 @@
+import Level from './Level';
+
+import imgN64 from '../assets/n64.jpeg';
+import imgGc from '../assets/gc.jpeg';
+import imgPs1 from '../assets/ps1.jpeg';
+
+const levels = [
+  new Level('n64', imgN64),
+  new Level('gc', imgGc),
+  new Level('ps1', imgPs1),
+];
+
+export default levels;
