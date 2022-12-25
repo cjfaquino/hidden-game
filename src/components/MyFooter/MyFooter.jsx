6a7b17529874a645020bed47833ee55a71ab1bf0
Christian Aquino <cjfaquino@gmail.com>
2022-12-24 23:33:49 -0800

    change footer to light

--- src/components/MyFooter/MyFooter.jsx
+++ src/components/MyFooter/MyFooter.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './MyFooter.css';
-import Logo from './GitHub-Mark-Light-64px.png';
+import Logo from './github-mark.png';
+import lightLogo from './GitHub-Mark-Light-64px.png';
 
-const MyFooter = () => (
-  <footer>
-    Copyright © 2022 <a href='https://github.com/cjfaquino/'>cjfaquino</a>
-    <img src={Logo} alt='github user: cjfaquino' />
-  </footer>
-);
+function MyFooter() {
+  return (
+    <footer>
+      Copyright © 2022 <a href='https://github.com/cjfaquino/'>cjfaquino</a>
+      <img src={Logo} alt='github user: cjfaquino' />
+    </footer>
+  );
+}
 
 export default MyFooter;
