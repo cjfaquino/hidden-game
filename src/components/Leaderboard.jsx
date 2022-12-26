8dac92c994406efb7c5bc8e6340edd7ce4c41530
Christian Aquino <cjfaquino@gmail.com>
2022-12-25 22:50:27 -0800

    display selected leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import levels from './levels';
 import MyNav from './MyNav';
 
 function Leaderboard() {
+  const params = useParams();
+  const [currentItem, setCurrentItem] = useState(null);
+
+  const getLevel = (name) => {
+    const [filtered] = levels.filter((level) => level.name.short === name);
+    return filtered;
+  };
+
+  // get scores from firebase
+
+  useEffect(() => {
+    const level = getLevel(params.name);
+    if (typeof level === 'object') setCurrentItem(level);
+    return () => {};
+  }, []);
+
   return (
     <>
       <MyNav />
-      <div>Leadboard</div>
+      <div>Leaderboard</div>
+
+      {currentItem && (
+        <div className='board-img-container'>
+          <img src={currentItem.thumbUrl} alt={currentItem.name.long} />
+        </div>
+      )}
     </>
   );
 }
