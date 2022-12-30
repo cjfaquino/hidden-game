f6c7e7a19796bacfab4428a951d5ce41f7a6afbc
Christian Aquino <cjfaquino@gmail.com>
2022-12-29 22:49:32 -0800

    add loading

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -4,12 +4,14 @@ import levels from './levels';
 import MyNav from './MyNav';
 import { getScores } from '../firebase';
 import LeaderboardItem from './LeaderboardItem';
+import Loading from './Loading/Loading';
 
 function Leaderboard() {
   const params = useParams();
   const location = useLocation();
   const [currentItem, setCurrentItem] = useState(null);
-  const [scores, setScores] = useState(new Array(10));
+  const [scores, setScores] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getLevel = (name) => {
     const [filtered] = levels.filter((level) => level.name.short === name);
@@ -31,6 +33,7 @@ function Leaderboard() {
       (async () => {
         const scoresArr = await getScores(currentItem.name.short);
         setScores(scoresArr);
+        setLoading(false);
       })();
     }
   }, [currentItem]);
@@ -45,24 +48,26 @@ function Leaderboard() {
             <img src={currentItem.thumbUrl} alt={currentItem.name.long} />
           </div>
         )}
+        {loading && <Loading />}
+        {!loading && (
+          <div className='top10'>
+            <h2>Top 10 Scores</h2>
+            <div className='scoreboard-grid'>
+              <div className='label-scoreboard-rank'>#</div>
+              <div className='label-scoreboard-user'>Name</div>
+              <div className='label-scoreboard-score'>Duration</div>
+              <div className='label-scoreboard-date'>Date</div>
 
-        <div className='top10'>
-          <h2>Top 10 Scores</h2>
-          <div className='scoreboard-grid'>
-            <div className='label-scoreboard-rank'>#</div>
-            <div className='label-scoreboard-user'>Name</div>
-            <div className='label-scoreboard-score'>Duration</div>
-            <div className='label-scoreboard-date'>Date</div>
-            {scores.length > 0 &&
-              scores.map((obj, index) => (
+              {scores.map((obj, index) => (
                 <LeaderboardItem
                   key={crypto.randomUUID()}
                   obj={obj}
                   index={index}
                 />
               ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
