943834a14fdc9b928d21f173cd316e95bded08ea
Christian Aquino <cjfaquino@gmail.com>
2022-12-31 23:40:42 -0800

    add home header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,24 +5,28 @@ import levels from './levels';
 
 function Home() {
   return (
-    <div className='home'>
-      {levels.map((level) => {
-        const { name, thumbUrl, thumbLoadingUrl } = level;
-
-        return (
-          <div className='home-img-container' key={crypto.randomUUID()}>
-            <Link to={`${name.short}`} state={level}>
-              {/* <img src={thumbUrl} alt={name.long} /> */}
-              <ProgressiveImg
-                placeholderSrc={thumbLoadingUrl}
-                src={thumbUrl}
-                alt={name.long}
-              />
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <header>
+        <h1>Level select</h1>
+      </header>
+      <div className='home'>
+        {levels.map((level) => {
+          const { name, thumbUrl, thumbLoadingUrl } = level;
+          return (
+            <div className='home-img-container' key={crypto.randomUUID()}>
+              <Link to={`${name.short}`} state={level}>
+                {/* <img src={thumbUrl} alt={name.long} /> */}
+                <ProgressiveImg
+                  placeholderSrc={thumbLoadingUrl}
+                  src={thumbUrl}
+                  alt={name.long}
+                />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
