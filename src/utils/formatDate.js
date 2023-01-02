4274fd8235d03f393c47dbe561e50e0f8ca14053
Christian Aquino <cjfaquino@gmail.com>
2023-01-01 19:18:44 -0800

    change date format

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,13 @@
 const formatDate = (secs) => {
   const t = new Date(Date.UTC(1970, 0, 1)); // Epoch
   t.setUTCSeconds(secs);
-  return t.toLocaleDateString();
+
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  };
+  return t.toLocaleString('en-US', options);
 };
 
 export default formatDate;
